ca1ccad318e1d545d968fd8e79b0308d12a150b2
YoshiPark <yoshipark@yoshipark.lan>
2016-09-19 02:08:10 +0800

    code exercise

--- leetcode/58. Length of Last Word/58. Length of Last Word/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "Solution.h"
-int main() {
-	string s;
-	cin >> s;
-	Solution example;
-	cout << example.lengthOfLastWord(s) << endl;
-	return 0;
-}
--- leetcode/83. Remove Duplicates from Sorted List/83. Remove Duplicates from Sorted List/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-#include "Solution.h"
-using namespace std;
-int main() {
-
-}
